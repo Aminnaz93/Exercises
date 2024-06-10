e140d54328c32229ead7be4cb8ef89bf4175ee62
aminnaz93 <Aminnazari040@hotmail.com>
2024-06-10 07:56:52 +0200

    dad

--- main.py
+++ main.py
@@ -2008,3 +2008,14 @@ file = open("demofile.txt" , "r")
 print(file.read())'''
 
 #############################
+
+my_list = [1,2,3,4,5,6,7,8,9,10]
+
+my_list.reverse()
+
+for i in my_list:
+    print(i)
+
+
+###########################
+
