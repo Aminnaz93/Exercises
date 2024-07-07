7f6e008ed9921e37211171f69f2b75f644e58251
aminnaz93 <Aminnazari040@hotmail.com>
2024-07-07 10:57:55 +0200

    fcas

--- main.py
+++ main.py
@@ -2303,7 +2303,7 @@ print("You got it in", guesses, "guesses.")'''
     
 ######################################
 
-import random
+'''import random
 
 user_wins = 0
 computer_wins = 0
@@ -2343,8 +2343,45 @@ while True:
 
 print("You won", user_wins, "times.")
 print("The computer won", computer_wins, "times.")
-print("Goodbye")
+print("Goodbye")'''
 
 
+###################################
+
+'''x = 10
+
+y = 100
+
+if x < y:
+    print("we are here now")
+elif y < x:
+    print("The second one now")
+else:
+    print("fuck u")'''
 
 
+##################################
+
+name = input("Type your name: ")
+
+print("Welcome", name, "to this adventure!")
+
+answer = input("You are on a dirt road, it has come to an end and you can go left or right. which way would you like to go? ").lower()
+
+if answer == "left":
+    answer = input("You come to a river, you can walk around it or swim across? type walk to walk and swim to seim accros: ")
+    
+    if answer == "swim":
+        print()
+    
+    elif answer == "walk":
+        print()
+
+    else: 
+        print("not a valid option. You lose. ")
+
+elif answer == "right":
+    print()
+
+else:
+    print("Not a valid option. You lose. ")
