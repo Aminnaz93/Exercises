ad764d67f69b739e3e71fde4acc87b06110de27a
aminnaz93 <Aminnazari040@hotmail.com>
2024-07-07 11:50:20 +0200

    cas

--- main.py
+++ main.py
@@ -2363,50 +2363,57 @@ else:
 ##################################
 
 name = input("Type your name: ")
-
 print("Welcome", name, "to this adventure!")
-
 print()
 
-answer = input("You are on a dirt road, it has come to an end and you can go left or right. which way would you like to go? ").lower()
+while True:
+    answer = input("You are on a dirt road, it has come to an end and you can go left or right. Which way would you like to go? ").lower()
 
-if answer == "left":
-    print()
-    answer = input("You come to a river, you can walk around it or swim across? type walk to walk and swim to swim accross: ")
-    
-    if answer == "swim":
-        print("You swam accross and were eaten by an alligator. ")
-    
-    elif answer == "walk":
-        print("You walked for many miles, ran out of water and you lost the game. ")
+    if answer == "left":
+        print()
+        answer = input("You come to a river, you can walk around it or swim across? Type 'walk' to walk and 'swim' to swim across: ").lower()
+        
+        if answer == "swim":
+            print("You swam across and were eaten by an alligator. You lose.")
+            break
+        
+        elif answer == "walk":
+            print("You walked for many miles, ran out of water and you lost the game.")
+            break
+        
+        else:
+            print("Not a valid option. Please enter 'walk' or 'swim'.")
+            continue
 
-    else: 
-        print("not a valid option. You lose. ")
+    elif answer == "right":
+        print()
+        answer = input("You come to a bridge, it looks wobbly, do you want to cross it or head back? (cross/back) ").lower()
 
-elif answer == "right":
-    print()
-    answer = input("You come to a bridge, it looks wobbly, do you want to cross it or head back? (cross/back) ").lower()
+        if answer == "back":
+            print("You go back and lose.")
+            break
 
-    if answer == "back":
-        print("You go back and lose. ")
+        elif answer == "cross":
+            answer = input("You cross the bridge and meet a stranger. Do you talk to them? (yes/no) ").lower()
 
-    elif answer == "cross":
-        answer = input("You cross the bridge and meet a stranger. Do you talk to them? (yes/no) ").lower()
+            if answer == "yes":
+                print("You talk to the stranger and they give you gold. You WIN!")
+                break
 
-        if answer == "yes":
-            print("You talk to the stranger and they give you gold. You WIN ")
+            elif answer == "no":
+                print("You ignore the stranger and they are offended and you lose.")
+                break
 
-        elif answer == "no":
-            print("You ignore the stranger and they are affended and you lose.")
+            else:
+                print("Not a valid option. Please enter 'yes' or 'no'.")
+                continue
 
         else:
-            print("Not a valid option. You lose.")
+            print("Not a valid option. Please enter 'cross' or 'back'.")
+            continue
 
+    else:
+        print("Not a valid option. Please enter 'left' or 'right'.")
+        continue
 
-    else: 
-        print("not a valid option. You lose. ")
-else:
-    print("Not a valid option. You lose. ")
-
-
-print("Thank you for trying", name)
+print("Thank you for playing", name)
