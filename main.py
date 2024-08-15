cf8275710cb84a4e3c91dee6b28b27bb602dd0fd
aminnaz93 <Aminnazari040@hotmail.com>
2024-08-15 18:39:15 +0200

    bj

--- main.py
+++ main.py
@@ -3043,7 +3043,7 @@ print(file.read())'''
 
 ####################################
 
-'''def print_board(board):
+def print_board(board):
     """Prints the Tic-Tac-Toe board."""
     print(" " + board[0] + " | " + board[1] + " | " + board[2])
     print("-----------")
@@ -3111,7 +3111,7 @@ def tic_tac_toe():
 
 
 if __name__ == "__main__":
-    tic_tac_toe()'''
+    tic_tac_toe()
 
 ####################################
 
@@ -3141,5 +3141,56 @@ if not found:
 
 ####################################
 
+'''import random
+
+def roll():
+    min_value = 1
+    max_value = 6
+    roll = random.randint(min_value,max_value)
+
+    return roll
+
+while True:
+
+    players = input("Enter the numbers of players (2-4): ")
+    
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            break
+        else:
+            print("Must be between 2-4 players.")
+
+    else:
+        print("Invalid, try again. ")
+
+max_score = 50
+player_scores = [0 for _ in range(players)]
+
+while max(player_scores) < max_score:
+
+    for player_idx in range(players):
+        print("\nPlayer number", player_idx + 1, "turn has just started!\n")
+        current_score = 0
+
+        while True:
+            should_roll = input("Would you like to roll (y)? ")
+            if should_roll.lower() != "y":
+                break
+
+            value = roll()
+            if value == 1:
+                current_score += value
+                print("You rolled a 1! Turn done")
+                current_score = 0
+                break
+            else:
+                print("You rolled a: ", value)
+
+            print("Your score is: ", current_score)
+
+        player_scores[player_idx] += current_score
+        print("Your total score is: ",player_scores[player_idx])'''
 
 
+####################################
