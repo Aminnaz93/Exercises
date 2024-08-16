9e363cf7c1e2d5d5dd9e56ab5644c2ccc975edf9
aminnaz93 <Aminnazari040@hotmail.com>
2024-08-16 16:59:36 +0200

    fsd

--- main.py
+++ main.py
@@ -3228,6 +3228,13 @@ if __name__ == "__main__":
     main()'''
 
 
-####################################
+###################################
 
+with open("story.txt" , 'r') as f:
+    story = f.read()
 
+words = []
+start_of_word = -1
+
+for i, char in enumerate(story):
+    
